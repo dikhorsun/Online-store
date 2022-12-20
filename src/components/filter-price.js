f6b76bd5a3121500aa4f74b886b9e3b3eb9daae0
AnastasiaShalukhina <exachka4@gmail.com>
2022-12-20 11:25:22 +0300

    fix: change styles and markup for filter-price at main page

--- src/components/filter-price.js
+++ src/components/filter-price.js
@@ -1,34 +1,36 @@
 'use strict';
-const inputLeft = document.querySelector('.input-left');
-const inputRight = document.querySelector('.input-right');
-const parent = document.querySelector('#slider-distance');
-const inputPartsContainer = document.querySelector('.input-parts-container');
-const inverseLeft = inputPartsContainer.querySelector('.inverse-left');
-const inverseRight = inputPartsContainer.querySelector('.inverse-right');
-const range = inputPartsContainer.querySelector('.range');
-const thumbLeft = inputPartsContainer.querySelector('.thumb-left');
-const thumbRight = inputPartsContainer.querySelector('.thumb-right');
-const signLeft = inputPartsContainer.querySelector('.sign-left');
-const signRight = inputPartsContainer.querySelector('.sign-right');
-const leftThumbScreen = signLeft.querySelector('.left-thumb-screen');
-const rightThumbScreen = signRight.querySelector('.right-thumb-screen');
-inputLeft.addEventListener('input', () => {
-    const rightValueMinusOne = inputRight.value - 1;
-    inputLeft.value = Math.min(inputLeft.value, rightValueMinusOne);
-    let value = (inputLeft.value / parseInt(inputLeft.max)) * 100;
-    inverseLeft.style.width = value + '%';
-    range.style.left = value + '%';
-    thumbLeft.style.left = value + '%';
-    signLeft.style.left = value + '%';
-    leftThumbScreen.innerHTML = inputLeft.value;
-});
-inputRight.addEventListener('input', () => {
-    inputRight.value = Math.max(inputRight.value, inputLeft.value - -1);
-    let value = (inputRight.value / parseInt(inputRight.max)) * 100;
-    // let children = this.parentNode.childNodes[1].childNodes;
-    inverseRight.style.width = 100 - value + '%';
-    range.style.right = 100 - value + '%';
-    thumbRight.style.left = value + '%';
-    signRight.style.left = value + '%';
-    rightThumbScreen.innerHTML = inputRight.value;
-});
+export function filterByPrice() {
+    const inputLeft = document.querySelector('.input-price-left');
+    const inputRight = document.querySelector('.input-price-right');
+    const inputPartsContainer = document.querySelector('.filter-price__visible-parts-container');
+    const inverseLeft = inputPartsContainer.querySelector('.filter-price__inverse-left');
+    const inverseRight = inputPartsContainer.querySelector('.filter-price__inverse-right');
+    const range = inputPartsContainer.querySelector('.filter-price__range');
+    const thumbLeft = inputPartsContainer.querySelector('.filter-price__thumb-left');
+    const thumbRight = inputPartsContainer.querySelector('.filter-price__thumb-right');
+    const signLeft = inputPartsContainer.querySelector('.filter-price__sign-left');
+    const signRight = inputPartsContainer.querySelector('.filter-price__sign-right');
+    const leftThumbScreen = signLeft.querySelector('.filter-price__left-thumb-screen');
+    const rightThumbScreen = signRight.querySelector('.filter-price__right-thumb-screen');
+
+    inputLeft.addEventListener('input', () => {
+        const rightValueMinusOne = inputRight.value - 1;
+        inputLeft.value = Math.min(inputLeft.value, rightValueMinusOne);
+        let value = (inputLeft.value / parseInt(inputLeft.max)) * 100;
+        inverseLeft.style.width = value + '%';
+        range.style.left = value + '%';
+        thumbLeft.style.left = value + '%';
+        signLeft.style.left = value + '%';
+        leftThumbScreen.innerHTML = inputLeft.value;
+    });
+    inputRight.addEventListener('input', () => {
+        inputRight.value = Math.max(inputRight.value, inputLeft.value - -1);
+        let value = (inputRight.value / parseInt(inputRight.max)) * 100;
+        inverseRight.style.width = 100 - value + '%';
+        range.style.right = 100 - value + '%';
+        thumbRight.style.left = value + '%';
+        signRight.style.left = value + '%';
+        rightThumbScreen.innerHTML = inputRight.value;
+    });
+}
+filterByPrice();
