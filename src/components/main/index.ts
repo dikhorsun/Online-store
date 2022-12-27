28a3f37ae54279cf360230182209b14a6b21c443
dikhorsun <dmitry.horsun@gmail.com>
2022-12-27 22:53:27 +0300

    feat: add routing 4 page + smth.

--- src/components/main/index.ts
+++ src/components/main/index.ts
@@ -10,12 +10,21 @@ class MainPage extends Page {
     }
 
     //   renderMainPage() {
-    //  вот здесь прописать код по рендерингу
+    //  вот здесь прописать код по рендерингу, например
+    // const pageButtons = document.createElement('div');
+    // Buttons.forEach((button) => {
+    //   const buttonHTML = document.createElement('a');
+    //   buttonHTML.href = `#${button.id}`;
+    //   buttonHTML.innerText = button.text;
+    //   pageButtons.append(buttonHTML);
+    // });
+    // this.container.append(pageButtons);
     // }
 
     render() {
         const title = this.createHeaderTitle(MainPage.TextObject.MainTitle);
         this.container.append(title);
+        // а здесь вместо 2-х строчек выше просто вызываешь метод this.renderMainPage()
         return this.container;
     }
 }
